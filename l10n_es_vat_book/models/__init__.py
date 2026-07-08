17a9f5673065e2d21acdbf645b1ba9a1ca8960b9
Rafael Blasco <rafaelbn@users.noreply.github.com>
2026-07-08 09:41:53 +0200

    [FIX] l10n_es_vat_book: revert extra import

--- l10n_es_vat_book/models/__init__.py
+++ l10n_es_vat_book/models/__init__.py
@@ -1,6 +1,5 @@
 from . import aeat_vat_book_map_line
 from . import l10n_es_vat_book
-from . import l10n_es_vat_book_group_taxes
 from . import l10n_es_vat_book_line
 from . import l10n_es_vat_book_line_tax
 from . import l10n_es_vat_book_summary
